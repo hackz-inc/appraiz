75756d3b0f6a180fb7af069e92175eb74e42a760
uuronn <uuronprogram@gmail.com>
2026-03-24 00:38:43 +0900

    GuestAuthGuardコンポーネントの追加: ゲストユーザー向けの認証ガードを実装

---
+++ src/components/guards/GuestAuthGuard.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { AuthGuard } from './AuthGuard'
+
+interface GuestAuthGuardProps {
+  children: React.ReactNode
+}
+
+export const GuestAuthGuard = ({ children }: GuestAuthGuardProps) => {
+  return (
+    <AuthGuard requiredRole="guest" redirectTo="/guest/auth/login">
+      {children}
+    </AuthGuard>
+  )
+}
