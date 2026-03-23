841abc399528bd2198628381217ab648fe252e08
uuronn <uuronprogram@gmail.com>
2026-03-23 23:41:33 +0900

    テーマスタイルの追加: ブレークポイント、カラー、フォント、オーバーレイ順序の定義を新規作成

---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  yellow: {
+    primary: "var(--yellow)",
+    lighten: {
+      1: "var(--yellow-lighten-1)",
+      2: "var(--yellow-lighten-2)"
+    }
+  },
+  blue: "var(--blue)",
+  red: "var(--red)",
+  green: "var(--green)",
+  black: {
+    primary: "var(--black)",
+    lighten: {
+      1: "var(--black-lighten-1)",
+      2: "var(--black-lighten-2)",
+      3: "var(--black-lighten-3)",
+      4: "var(--black-lighten-4)",
+      5: "var(--black-lighten-5)"
+    }
+  },
+  white: "var(--white)"
+};
