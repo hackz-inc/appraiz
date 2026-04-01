5d4ee9eff67ebbb97f61410af9258a6d54cb850b
uuronn <uuronprogram@gmail.com>
2026-04-01 20:53:16 +0900

    feat: ローディングアイコンコンポーネントを追加

---
+++ src/components/ui/LoadingIcon/index.tsx
@@ -0,0 +1,5 @@
+export const LoadingIcon = () => (
+	<div className="w-full h-full p-8 flex items-center justify-center">
+		<div className="w-24 h-24 border-4 border-white border-t-black rounded-full animate-spin" />
+	</div>
+);
